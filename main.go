d7b01adb975c27b099831ae42d9dcf65aefb0bca
holysabre <1026579245@qq.com>
2023-06-19 16:54:35 +0800

    feat:修改为从env读取配置

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"collector-backend/pkg/crontab"
 	"collector-backend/pkg/rabbitmq"
 	"collector-backend/util"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -12,7 +16,19 @@ func main() {
 }
 
 func run() {
-	config := rabbitmq.Config{Url: "amqp://root:password@192.168.88.112:5672/"}
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("无法加载 .env 文件")
+	}
+
+	// 访问环境变量
+	amqpUsername := os.Getenv("RABBITMQ_USERNAME")
+	amqpPassowd := os.Getenv("RABBITMQ_PASSWORD")
+	amqpUrl := os.Getenv("RABBITMQ_URL")
+
+	url := "amqp://" + amqpUsername + ":" + amqpPassowd + "@" + amqpUrl
+
+	config := rabbitmq.Config{Url: url}
 	conn, err := rabbitmq.NewConnection(config)
 	util.LogIfErr(err)
 	defer conn.Conn.Close()
