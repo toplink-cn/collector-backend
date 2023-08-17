b83f206f27701974fb5a9cb73350a9c0f0c42e7b
ywj <1026579245@qq.com>
2023-08-17 18:16:43 +0800

    test

--- db/redis.go
+++ db/redis.go
@@ -3,15 +3,23 @@ package db
 import (
 	"collector-backend/util"
 	"context"
+	"runtime"
 
 	"github.com/go-redis/redis/v8"
 )
 
+const PoolCapPreCoreNum = 10
+
 func GetRedisConnection() *redis.Client {
+
+	numCPU := runtime.NumCPU()
+	poolCap := PoolCapPreCoreNum * numCPU
+
 	client := redis.NewClient(&redis.Options{
-		Network: "unix",
-		Addr:    "/app/run/redis.sock",
-		DB:      0,
+		Network:  "unix",
+		Addr:     "/app/run/redis.sock",
+		DB:       0,
+		PoolSize: int(poolCap),
 	})
 
 	// 使用 Ping() 方法检查是否成功连接到 Redis
